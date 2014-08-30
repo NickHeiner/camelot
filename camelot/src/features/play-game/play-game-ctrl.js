8652bc3ce7c2fbffe6358b91b5b12ec761d3b1cf
Nick Heiner <nth23@cornell.edu>
2014-08-30 18:00:37 -0400

    fleshing out active moves functionality more

--- camelot/src/features/play-game/play-game-ctrl.js
+++ camelot/src/features/play-game/play-game-ctrl.js
@@ -54,30 +54,32 @@ angularModule.controller('PlayGameCtrl', function ($scope, $routeParams, bindMod
                 knight: boardSpacePieceMatches(row, col, 'type', camelotConstants.KNIGHT),
                 pawn: boardSpacePieceMatches(row, col, 'type', camelotConstants.PAWN),
 
-                'possible-move': _.noop,
-                'active-move': spaceIsInActiveMoves(row, col)
+                'possible-move': boardSpaceIsInPossibleMoves(row, col),
+                'active-move': boardSpaceIsInActiveMoves(row, col)
             };
         }
 
-        function spaceIsInActiveMoves(row, col) {
+        function boardSpaceIsInPossibleMoves(row, col) {
+            // This length check is necessary until https://github.com/NickHeiner/camelot-engine/issues/8 is resolved
+            return $scope.activeMoveCoords.length && camelotQuery.isValidMove(game.gameState, $scope.activeMoveCoords.concat({row: row, col: col}));
+        }
+
+        function boardSpaceIsInActiveMoves(row, col) {
             return _.find($scope.activeMoveCoords, { row: row, col: col });
         }
 
         function onClickBoardSpace(row, col) {
-            var isPieceForCurrentPlayer = boardSpacePieceMatches(row, col, 'player', playerForCurrentUser),
-                currCoord;
+            var isPieceForCurrentPlayer = boardSpacePieceMatches(row, col, 'player', playerForCurrentUser);
 
             if (!isPieceForCurrentPlayer && !$scope.activeMoveCoords.length) {
                 return;
             }
 
-            currCoord = { row: row, col: col };
-
-            if (!camelotQuery.isValidMove(game.gameState, $scope.activeMoveCoords.concat(currCoord))) {
+            if (!camelotQuery.isValidMove(game.gameState, $scope.activeMoveCoords.concat({ row: row, col: col }))) {
                 return;
             }
 
-            $scope.activeMoveCoords.push(currCoord);
+            $scope.activeMoveCoords.push({ row: row, col: col });
         }
 
         $scope.getBoardSpaceClasses = getBoardSpaceClasses;
