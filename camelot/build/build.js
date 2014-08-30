c8b5a36f4bc34d8aaec2238ba8d13ba5b7eccc89
Nick Heiner <nth23@cornell.edu>
2014-08-30 13:47:04 -0400

    fixing johnson box

--- camelot/build/build.js
+++ camelot/build/build.js
@@ -18592,7 +18592,7 @@ ngModule.controller('CamelotCtrl', function ($rootScope, auth, $scope, goToRoute
     $scope.getCurrentUser().then(function (user) {
         // We could use automate promise unwrapping here but that
         // feature is either deprecated already or will be soon.
-        $scope.user = user;
+        $scope.currentUser = user;
     });
 });
 },{"../angular-module":20}],22:[function(require,module,exports){
--- camelot/src/controllers/camelot-ctrl.js
+++ camelot/src/controllers/camelot-ctrl.js
@@ -8,6 +8,6 @@ ngModule.controller('CamelotCtrl', function ($rootScope, auth, $scope, goToRoute
     $scope.getCurrentUser().then(function (user) {
         // We could use automate promise unwrapping here but that
         // feature is either deprecated already or will be soon.
-        $scope.user = user;
+        $scope.currentUser = user;
     });
 });
